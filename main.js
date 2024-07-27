a3da6871067ca9b470a22b65196912619899a99e
Dario Jularic <dariojularic0@gmail.com>
2024-07-27 17:56:30 +0200

    Add style

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ import './style.css'
 const pokemonList = document.querySelector(".pokemon-list");
 const pokemonTypeList = document.querySelector(".pokemon-type-list");
 const selectedPokemon = document.querySelector(".selected-pokemon");
+// const pokemonImage = document.querySelector(".pokemon-image");
 
 const getPokemons = async () => {
-  const response = await fetch(`https://img.pokemondb.net/artwork/bulbasaur.jpg`)
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/1/`)
   console.log(response)
   const data = await response.json()
   console.log(data)
   return data
 }
 
+// .then(pokemonImage.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
 getPokemons()
   .then()
   .catch()
