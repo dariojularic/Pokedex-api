3d76ac7c08047ee5c75a7136ef0cbddc06948a11
Dario Jularic <dariojularic0@gmail.com>
2024-07-27 17:43:40 +0200

    Add fetch function

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+const pokemonList = document.querySelector(".pokemon-list");
+const pokemonTypeList = document.querySelector(".pokemon-type-list");
+const selectedPokemon = document.querySelector(".selected-pokemon");
+
+const getPokemons = async () => {
+  const response = await fetch(`https://img.pokemondb.net/artwork/bulbasaur.jpg`)
+  console.log(response)
+  const data = await response.json()
+  console.log(data)
+  return data
+}
+
+getPokemons()
+  .then()
+  .catch()
